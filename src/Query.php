c473b300c1c8a185f2ba5bc07f24f3ae5b865d84
Jens Richter <richter@marmalade.de>
2017-03-22 11:55:28 +0100

    Added query related data objects

---
+++ src/Query.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of a marmalade GmbH project
+ * It is not Open Source and may not be redistributed.
+ * For contact information please visit http://www.marmalade.de
+ * Version:    1.0
+ * Author:     Jens Richter <richter@marmalade.de>
+ * Author URI: http://www.marmalade.de
+ */
+
+namespace Makaira;
+
+use Kore\DataObject\DataObject;
+
+class Query extends DataObject
+{
+    /**
+     * @var string
+     */
+    public $searchPhrase;
+
+    /**
+     * @var array
+     */
+    public $contraints;
+
+    /**
+     * @var array
+     */
+    public $aggregations;
+
+    /**
+     * @var array
+     */
+    public $sorting;
+
+    /**
+     * @var integer
+     */
+    public $count;
+
+    /**
+     * @var integer
+     */
+    public $offset;
+}
---
+++ src/ResultItem.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of a marmalade GmbH project
+ * It is not Open Source and may not be redistributed.
+ * For contact information please visit http://www.marmalade.de
+ * Version:    1.0
+ * Author:     Jens Richter <richter@marmalade.de>
+ * Author URI: http://www.marmalade.de
+ */
+
+namespace Makaira;
+
+use Kore\DataObject\DataObject;
+
+class ResultItem extends DataObject
+{
+    /**
+     * @var string
+     */
+    public $id;
+}
