a7b454b7c83416b03c59d17735b910c69af44275
Jens Richter <richter@marmalade.de>
2017-03-23 16:33:05 +0100

    Added connection configuration object

---
+++ src/ConnectionConfiguration.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of a marmalade GmbH project
+ * It is not Open Source and may not be redistributed.
+ * For contact information please visit http://www.marmalade.de
+ * Version:    1.0
+ * Author:     Jens Richter <richter@marmalade.de>
+ * Author URI: http://www.marmalade.de
+ */
+
+namespace Makaira;
+
+use Kore\DataObject\DataObject;
+
+class ConnectionConfiguration extends DataObject
+{
+    public $url;
+    public $secret;
+}
