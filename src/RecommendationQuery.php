928715b0b3996439a20f5f4ad3f4680ef7fce744
Stefan Krenz <krenz@marmalade.de>
2018-05-04 18:49:26 +0200

    MAKAIRA-492 Added recommendation query.

---
+++ src/RecommendationQuery.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of a marmalade GmbH project
+ * It is not Open Source and may not be redistributed.
+ * For contact information please visit http://www.marmalade.de
+ *
+ * @version 0.1
+ * @author  Stefan Krenz <krenz@marmalade.de>
+ * @link    http://www.marmalade.de
+ */
+
+namespace Makaira;
+
+class RecommendationQuery extends AbstractQuery
+{
+    /**
+     * @return string[]
+     */
+    public function getMandatoryConstraints(): array
+    {
+        return [
+            Constraints::LANGUAGE => 'language',
+            Constraints::SHOP => 'shop identifier',
+            Constraints::RECOMMENDATION => 'recommendation identifier',
+            Constraints::RECOMMENDATION_REQUEST => 'request identifier',
+        ];
+    }
+
+    public function verify()
+    {
+        parent::verify();
+
+        if (null === $this->count) {
+            throw new \DomainException('Count must be set!');
+        }
+
+        if (1 > $this->count) {
+            throw new \DomainException('Count must be greater than 0!');
+        }
+    }
+}
