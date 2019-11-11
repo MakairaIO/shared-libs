f5cd6c71da428736f7e1be898f301d64e53a6c9d
Thomas Uhlig <uhlig@marmalade.de>
2019-11-11 12:06:18 +0100

    MAKAIRA-887 added connect exceptions

---
+++ src/Connect/Exceptions/OutOfBoundsException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of a marmalade GmbH project
+ * It is not Open Source and may not be redistributed.
+ * For contact information please visit http://www.marmalade.de
+ * Version:    1.0
+ * Author:     Florian Ludwig <ludwig@marmalade.de>
+ * Author URI: http://www.marmalade.de
+ */
+
+namespace Makaira\Connect\Exceptions;
+
+use Makaira\Connect\Exception as ConnectException;
+
+class OutOfBoundsException extends ConnectException
+{
+
+}
