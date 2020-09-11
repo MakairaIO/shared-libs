34a8f96fbe91e0ed8d466bb1e167450ad00561fd
Rafael Friesen <rf@marmalade.group>
2020-09-11 09:15:19 +0200

    Add Exception ID

--- src/Exception.php
+++ src/Exception.php
@@ -14,5 +14,11 @@ use Exception as BaseException;
 
 class Exception extends BaseException
 {
+    public $exceptionId;
 
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->exceptionId = uniqid('MEX-', true);
+    }
 }
