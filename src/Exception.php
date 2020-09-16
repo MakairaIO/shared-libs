d29f285de58eeaed9fb2af1c86301679f3a06687
Rafael Friesen <rf@marmalade.group>
2020-09-16 13:30:02 +0200

    Rename exceptionId to errorId

--- src/Exception.php
+++ src/Exception.php
@@ -14,11 +14,11 @@ use Exception as BaseException;
 
 class Exception extends BaseException
 {
-    public $exceptionId;
+    public $errorId;
 
     public function __construct($message = "", $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
-        $this->exceptionId = uniqid('MEX-', true);
+        $this->errorId = uniqid('MEX-', true);
     }
 }
