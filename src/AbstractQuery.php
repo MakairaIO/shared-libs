80e1735b1c14e1d4ba2ec2a712af91277ceef7d6
Sunny <dt@marmalade.group>
2024-04-08 14:51:55 +0700

    MAKAIRA-4359 Apply feedback from PR

--- src/AbstractQuery.php
+++ src/AbstractQuery.php
@@ -69,7 +69,7 @@ abstract class AbstractQuery extends DataObject
     public function verify()
     {
         foreach ($this->getMandatoryConstraints() as $key => $label) {
-            if (!$this->isConstraintExists($key)) {
+            if (empty($this->constraints[$key])) {
                 throw new DomainException(sprintf('Missing mandatory %s constraint.', $label));
             }
         }
@@ -115,18 +115,5 @@ abstract class AbstractQuery extends DataObject
         return (new static($data))
             ->verify()
             ->filterConstraints();
-    }
-
-    private function isConstraintExists($key)
-    {
-        if (!isset($this->constraints[$key])) {
-            return false;
-        }
-
-        if ('' === $this->constraints[$key]) {
-            return false;
-        }
-
-        return true;
     }
 }
